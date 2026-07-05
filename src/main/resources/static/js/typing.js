c325c46ea16e984f2144d1888ed3dc2bb467531c
Travis Gooden <tag7+sfu@sfu.ca>
2026-07-04 22:45:58 -0700

    feat: add function to populate text buffer and variables to track buffer state

--- src/main/resources/static/js/typing.js
+++ src/main/resources/static/js/typing.js
@@ -1,13 +1,19 @@
 const PLACEHOLDER_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 const CHUNK_SIZE = 20; // the number of words per chunk
+const BUFFER_THRESHOLD = 3; // signals refilling when buffer drops below this size
+const TARGET_BUFFER_SIZE = 5; // number of chunks to maintain in reserve
 
 const textBuffer = new Array();
+let currentChunkId = 0;
+let activeChunk = null; // represents the object currently being typed
 
-// this doesn't need to be async, but dealing with promises makes the logic more consistent for when the backend is ready.
-//
+// Temporary function to simulate fetching text from an API and returning it in chunks
+//	
 // Params: chunkId - an integer representing a place in the blob of text we are grabbing from
 // Returns: an object in the form { chunkId: chunkId, text: [...]} where "text" is an array of individual characters
+//
+// Note: this doesn't need to be async, but dealing with promises makes the logic more consistent for when the backend is ready.
 async function fetchFakeText(chunkId) {
 	let words = PLACEHOLDER_TEXT.split(" ");
 	let startPosition = (chunkId * CHUNK_SIZE) % words.length;
@@ -27,7 +33,21 @@ async function fetchFakeText(chunkId) {
 	};
 }
 
-function setup() {
+// Refills the buffer when it gets too low
+async function populateBuffer() {
+	while (textBuffer.length < TARGET_BUFFER_SIZE) {
+		const newChunk = await fetchFakeText(currentChunkId);
+		textBuffer.push(newChunk);
+		currentChunkId++;
+	}
+}
+
+async function setup() {
+	await populateBuffer();
+
+	// pull the first chunk from the front of the array
+	activeChunk = textBuffer.shift();
+
 	let typingInterface = document.getElementById("typing-interface");
 	typingInterface.textContent = PLACEHOLDER_TEXT;
 }
